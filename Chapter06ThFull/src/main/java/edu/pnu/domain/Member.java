f69149b8cd28ee6a80902715ea72b786096e4c05
Dnadit <yuhyun.kang07@gmail.com>
2023-05-04 18:03:11 +0900

    version down to 2.7.11

--- Chapter06ThFull/src/main/java/edu/pnu/domain/Member.java
+++ Chapter06ThFull/src/main/java/edu/pnu/domain/Member.java
@@ -1,7 +1,9 @@
 package edu.pnu.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
